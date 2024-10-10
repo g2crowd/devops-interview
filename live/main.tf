770618573f6dfd7a5b2fdea75de8e2d8965483a3
bharathmreddy99 <bharathmreddy99@gmail.com>
2024-10-10 12:02:14 +0000

    Creating EKS resource alone with variable changes

--- live/main.tf
+++ live/main.tf
@@ -9,11 +9,11 @@ module "vpc" {
   version = "3.18.1"
 
   name = format("tf-%s-vpc",var.name)
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr
 
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = vpc_private_subnets
+  public_subnets  = vpc_public_subnets
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -23,3 +23,25 @@ module "vpc" {
   }
 }
 
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = format("tf-%s-eks-cluster",var.name)
+  cluster_version = "1.25"
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+
+      key_name = "devops-interview-key"
+    }
+  }
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.id
+}
--- live/variable.tf
+++ live/variable.tf
@@ -3,3 +3,25 @@ variable "name" {
     description = "Name of EKS infrastructure"
     default     = ""
 }
+
+variable "vpc_cidr" {
+    type        = string
+    description = "Cidr block for VPC'
+    default     = "10.0.0.0/16"
+}
+variable "azs" {
+    type        = list(string)
+    description = "AZs in which you prefer"
+    default     = []
+}
+variable "vpc_private_subnets" {
+    type        = list(string)
+    description = " Private subnet IP cidr"
+    default     = []
+}
+
+variable "vpc_public_subnets" {
+    type        = list(string)
+    description = " public subnet IP cidr"
+    default     = []
+}
